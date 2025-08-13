20a828e10a0c9eb8b241e10b4129e7d206a0a542
Brandon J. <68445378+BJennin4@users.noreply.github.com>
2025-08-12 20:13:41 -0400

    Basic style updates

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,59 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
 
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    // <div className="
+    //   grid grid-rows-[20px_1fr_20px]
+    //   items-center
+    //   justify-items-center
+    //   min-h-screen
+    //   p-8
+    //   pb-20
+    //   gap-16
+    //   sm:p-20
+    //   font-[family-name:var(--font-geist-sans)]">
+
+      <div className="
+        grid grid-rows-[20px_1fr_20px]
+        items-center
+        justify-items-center
+        min-h-screen
+        p-8
+        pb-20
+        gap-16
+        sm:p-20
+        font-[family-name:var(--font-geist-sans)]">
     <Head>
       <title>BranDev</title>
     </Head>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
+      <main className="
+        flex 
+        flex-col 
+        gap-8  
+        row-start-2 
+        items-center 
+        sm:items-start">
+        <div className="        
+          rounded-xl
+          shadow-lg 
+          outline 
+          outline-black/5
+          dark:shadow-none 
+          dark:outline-white/10
+          p-20
+          pt-10
+          pb-10">
           <h1 className="text-xl">Welcome to BranDev!</h1>
           <p className="pt-6">Homepage In Progress</p>
-          <a className="text-blue-500 hover:text-blue-700 font-semibold" href="https://github.com/BJennin4/personal_website" target="_blank">Right Here</a>
+          <a className="font-semibold" href="https://github.com/BJennin4/personal_website" target="_blank">Right Here</a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="
+        row-start-3 
+        flex 
+        gap-6 
+        flex-wrap 
+        items-center 
+        justify-center">
       </footer>
     </div>
   );
